d3d02a75ecafcd1c5e86c27af3c5d0e31d6ce12d
garlicfiendck <carolinekang@pursuit.org>
2019-04-02 21:21:30 -0400

    create video fragment layout

--- app/src/main/java/com/pursuit/nycmenagerie/VideoFragment.java
+++ app/src/main/java/com/pursuit/nycmenagerie/VideoFragment.java
@@ -1,8 +1,11 @@
 package com.pursuit.nycmenagerie;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,9 +18,19 @@ public class VideoFragment extends Fragment {
     public VideoFragment() {
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
     public static VideoFragment newInstance(){
         VideoFragment videoFragment = new VideoFragment();
         return videoFragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     @Override
@@ -26,4 +39,13 @@ public class VideoFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_video, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
 }
